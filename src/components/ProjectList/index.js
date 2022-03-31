e8e6e1830fa6241dfa34d2265649138c0004669f
Shaun Sticka <shaun.sticka@outlook.com>
2022-03-31 11:59:54 -0500

    fixed sidebar to permanent display on click and added icon click functionality to projects

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -1,13 +1,24 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 function ProjectUse(props) {
 
+
     return (
         <div key={props.project.name} className="project__image--container container my-4">
-            <a href="https://www.google.com" className="project__image--overlay">
-                <h5>
-                    We are awesome
-                </h5>
+            <a href={props.project.liveLink} target="_blank" rel="noreferrer" className="project__image--overlay">
+                <h1>
+                  {props.project.description}
+                </h1>
+                <br />
+                <h2>
+                    {props.project.depth}
+                </h2>
+                <br />
+                <a href={props.project.gitLink} target="_blank" rel="noreferrer">
+                                    <FontAwesomeIcon className="fa-2xl transition linear delay-100 hover:scale-125 hover:animate-spin 1s" icon={faGithub}></FontAwesomeIcon>
+                                    </a>
                 </a>
 
                 <img
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,51 +1,67 @@
 import React from "react";
 import ProjectUse from "../ProjectList";
 
+
 function Projects() {
     const myProjects = [
         {
             image: "../../assets/projects/beyond-the-peak.jpg",
             name: "beyond-the-peak.jpg",
-            description: "Random drink generator",
-            link: "https://github.com/glanctot/beyond-the-peak"
+            description: "Going out with the bois but tired of the same-old-same-old? This app generates a random drink, guaranteed to add some random fun to your evening. Level up by adding extra randomness if you dare.",
+            depth: "Full-Stack with MySQL database",
+            liveLink: "https://pacific-journey-17528.herokuapp.com/",
+            gitLink: "https://github.com/glanctot/beyond-the-peak"
+          },
+          {
+            image: "../../assets/projects/dinder.png",
+            name: "dinder.png",
+            description: "Designed for the person who wants a dog but just has no clue what kind of dog or where to start learning. A true entry-level app into the world of dog-ownership. It's like Tinder... for people seeking a new fur-friend.",
+            depth: "MERN Stack",
+            liveLink: "https://dinder-dog.herokuapp.com/",
+            gitLink: "https://github.com/mnshaun24/doggy-days"
           },
           {
             image: "../../assets/projects/just-tech-it.jpg",
             name: "just-tech-it.jpg",
-            description: "Simple tech blog",
-            link: "https://github.com/mnshaun24/just-tech-it"
+            description: "A tech blog for people to share information and resources related to the tech industry.",
+            depth: "Full-Stack",
+            liveLink: "https://just-tech-it.herokuapp.com/",
+            gitLink: "https://github.com/mnshaun24/just-tech-it"
           },
           {
             image: "../../assets/projects/weather-for-you.jpg",
             name: "weather-for-you.jpg",
-            description: "Weather forecast app",
-            link: "https://github.com/mnshaun24/weather-for-you"
+            description: "Check the weather and forecast in your local area or anywhere in the US. Utilizes local storage to save recently searched cities.",
+            depth: "Front-End",
+            liveLink: "https://mnshaun24.github.io/weather-for-you/",
+            gitLink: "https://github.com/mnshaun24/weather-for-you"
           },
           {
             image: "../../assets/projects/biz-notes.jpg",
             name: "biz-notes.jpg",
-            description: "Note taker in readme format",
-            link: "https://github.com/mnshaun24/biz-notes"
+            description: "For business professionals who want an easy, digestable note taker with a clean interface",
+            depth: "Full-Stack",
+            liveLink: "",
+            gitLink: "https://github.com/mnshaun24/biz-notes"
           },
           {
             image: "../../assets/projects/readme.jpg",
             name: "readme.jpg",
-            description: "Readme generator for coding projects",
-            link: "https://github.com/mnshaun24/ez-reader"
+            description: "Generates ReadMe files easily for projects. All functionality is through the terminal and ReadMe is auto generated after answering the prompts.",
+            depth: "Back-End",
+            liveLink: "https://biz-notes.herokuapp.com/",
+            gitLink: "https://github.com/mnshaun24/ez-reader"
           },
           {
             image: "../../assets/projects/scheduler.jpg",
             name: "scheduler.jpg",
-            description: "Daily planner used for organization",
-            link: "https://github.com/mnshaun24/make-a-date-daily-planner"
-          },
-          {
-            image: "../../assets/projects/run-buddy.jpg",
-            name: "run-buddy.jpg",
-            description: "Front-end example",
-            link: "https://github.com/mnshaun24/run-buddy"
+            description: "Simple scheduler for daily organization. Create, save, and edit tasks in the easy-to-understand interface.",
+            depth: "Full-Stack",
+            liveLink: "https://mnshaun24.github.io/make-a-date-daily-planner/",
+            gitLink: "https://github.com/mnshaun24/make-a-date-daily-planner"
           }
-    ]
+
+              ]
 
       // return (
       //   <div className="container mx-auto grid grid-cols-2 space-y-4 space-x-3">
